2ca81560b0a9ca7081608d6d392bb1f721f5fca0
Webber Wu <webberwu.tw@gmail.com>
2024-10-23 14:21:53 +0800

    add `<leader>fh` keymap to find hidden files

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -21,6 +21,8 @@ keymap.set("n", "mh", "<cmd>nohlsearch<cr>", { noremap = true })
 keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>")
 
 -- nvim-telescope
+-- https://github.com/nvim-telescope/telescope.nvim/blob/82e3cc322ad87b262aef092cb7475e769740e83a/lua/telescope/builtin/files.lua#L167-L184
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>")
+keymap.set("n", "<leader>fh", "<cmd>Telescope find_files hidden=true no_ignore=true<cr>")
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
 keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
